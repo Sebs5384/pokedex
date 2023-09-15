738bda9cba8473499954c9ca85aaed426effe82e
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-14 23:58:15 -0600

    Creates pagination
    This function is in charge of creating the pagination, we also adding a previos and next button by the end of each side of the paginator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-function initialize() {}
+import { getPokemonPaginationData } from "./api/pokemon.js";
+function initialize() {
+  getPokemonPaginationData().then((data) => {
+    const { names, next, previous, count } = data;
+
+    const limit = "20";
+    const totalPokemons = count;
+    const pages = Math.ceil(totalPokemons / limit + 1);
+    let currentPage = 1;
+
+    createPaginator(pages);
+  });
+}
+
+function createPaginator(pages) {
+  const $page = document.querySelector("#paginator-container");
+
+  for (let i = 0; i <= pages; i++) {
+    const $item = document.createElement("li");
+    const $link = document.createElement("a");
+
+    $item.className = "page-item";
+    $link.className = "page-link card-border paginator-text";
+    $link.href = "#";
+
+    if (i === 0) {
+      $link.textContent = "Previous";
+    } else if (i === pages) {
+      $link.textContent = "Next";
+    } else {
+      $link.textContent = i;
+    }
+
+    $item.appendChild($link);
+    $page.appendChild($item);
+  }
+}
 
 initialize();
