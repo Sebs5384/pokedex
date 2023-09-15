92e423ffe80bf2b060291e8df43b532068e0461a
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-14 22:29:06 -0600

    Update apiPokemon
    This introduces a new function that will be in charge of returning the pagination data we'll use later to create our pagination navbar

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,6 +1,6 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
-export function getPokemon(pokemon) {
+function getPokemon(pokemon) {
   const pokemonURL = `${URL}${pokemon}`;
 
   return fetch(pokemonURL)
@@ -12,7 +12,7 @@ export function getPokemon(pokemon) {
     });
 }
 
-export function getPokemons(limit, offset) {
+function getPokemons(limit = "20", offset = "0") {
   const pokemonsURL = `${URL}?limit=${limit}&offset=${offset}`;
 
   return fetch(pokemonsURL)
@@ -22,4 +22,15 @@ export function getPokemons(limit, offset) {
     .then((pokemons) => {
       return pokemons;
     });
+}
+
+export function getPokemonPaginationData(limit, offset) {
+  return getPokemons(limit, offset).then((pokemons) => {
+    return {
+      names: pokemons.results.map((pokemon) => pokemon.name),
+      next: pokemons.next,
+      previous: pokemons.previous,
+      count: pokemons.count,
+    };
+  });
 }
