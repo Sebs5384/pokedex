f81384c29a8b16fdfa51ea25403d3cc084319dad
Sebs5384 <operationmadjackal@hotmail.com>
2023-09-16 21:35:36 -0300

    Update pagination
    setup the pagination

--- src/ui/pagination.js
+++ src/ui/pagination.js
@@ -1,24 +1,52 @@
 import { getPokemonPaginationData } from "../api/pokemon.js";
 
-export function setupPagination() {
-  getPokemonPaginationData().then((data) => {
+export function updatePagination(POKEMONS_PER_PAGE, NEXT_PAGE) {
+  getPokemonPaginationData(POKEMONS_PER_PAGE, NEXT_PAGE).then((data) => {
     const { names, next, previous, count } = data;
+    const TOTAL_POKEMONS = count;
+    const TOTAL_PAGES = Math.ceil(TOTAL_POKEMONS / POKEMONS_PER_PAGE + 1);
+    const CURRENT_PAGE = NEXT_PAGE / POKEMONS_PER_PAGE;
 
-    const limit = 20;
-    const totalPokemons = count;
-    const totalPages = Math.ceil(totalPokemons / limit + 1);
-    let currentPage = 1;
-
-    createPaginator(totalPages);
-    updatePaginatorPages(currentPage);
-    updatePagination();
+    console.log(data);
+    handlePaginationChanges(POKEMONS_PER_PAGE);
+    createPaginator(TOTAL_PAGES);
+    setCurrentPages(CURRENT_PAGE);
   });
 }
 
-function createPaginator(pages) {
-  const $page = document.querySelector("#paginator-container");
+function setCurrentPages(current) {
+  const $pages = document.querySelectorAll(".paginator-page");
 
-  for (let i = 0; i <= pages; i++) {
+  $pages.forEach((page, index) => {
+    const pageIndex = index + 1;
+
+    const currentPage = Number(current);
+    pageIndex >= currentPage - 1 && pageIndex <= currentPage + 2 ? page.classList.remove("hidden") : page.classList.add("hidden");
+  });
+}
+
+function handlePaginationChanges(POKEMONS_PER_PAGE) {
+  const $buttons = document.querySelector("#paginator-container");
+
+  $buttons.onclick = (event) => {
+    event.preventDefault();
+    const $clickedElement = event.target;
+
+    if ($clickedElement.classList.contains("paginator-page")) {
+      const CURRENT_PAGE = Number(event.target.innerText);
+      console.log(CURRENT_PAGE, "currentpage");
+      const NEXT_PAGE = (CURRENT_PAGE - 1) * POKEMONS_PER_PAGE;
+
+      updatePagination(POKEMONS_PER_PAGE, NEXT_PAGE);
+    }
+  };
+}
+
+function createPaginator(totalPages) {
+  const $pageContainer = document.querySelector("#paginator-container");
+
+  $pageContainer.innerHTML = "";
+  for (let i = 0; i <= totalPages; i++) {
     const $item = document.createElement("li");
     const $link = document.createElement("a");
 
@@ -26,36 +54,10 @@ function createPaginator(pages) {
     $link.className = "page-link card-border paginator-text";
     $link.href = "#";
 
-    $link.textContent = i === 0 ? "Previous" : i === pages ? "Next" : i;
-    i !== 0 && i !== pages ? $link.classList.add("paginator-pages") : null;
+    $link.textContent = i === 0 ? "Previous" : i === totalPages ? "Next" : i;
+    i !== 0 && i !== totalPages ? $link.classList.add("paginator-page") : null;
 
     $item.appendChild($link);
-    $page.appendChild($item);
+    $pageContainer.appendChild($item);
   }
-}
-
-function updatePaginatorPages(current) {
-  const $pages = document.querySelectorAll(".paginator-pages");
-
-  $pages.forEach((page, index) => {
-    const pageIndex = index + 1;
-    const currentPage = Number(current);
-
-    pageIndex >= currentPage - 2 && pageIndex <= currentPage + 2 ? page.classList.remove("hidden") : page.classList.add("hidden");
-  });
-}
-
-function handlePaginationClick(event) {
-  event.preventDefault();
-  const $clickedButton = event.target;
-  return $clickedButton.innerText;
-}
-
-function updatePagination() {
-  const $buttons = document.querySelector("#paginator-container");
-
-  $buttons.onclick = (event) => {
-    const $clickedButton = handlePaginationClick(event);
-    updatePaginatorPages($clickedButton);
-  };
 }
