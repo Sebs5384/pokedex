20ef997a284f7a27a2794dda3a0c359140eecba0
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-19 13:42:27 -0600

    Changes pokemon
    This will move the module outside the folder UI as it will nothing to do with the user interface

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,11 +1,11 @@
-import { getPokemons } from "../api/pokemon.js";
-import { updatePagination } from "./pagination.js";
-import { pokedexPaginationValues } from "../utils/general.js";
+import { getPokemons } from "./api/pokemon.js";
+import { setupPagination } from "./ui/pagination.js";
+import { calculatePaginationValues as pokedexPaginationValues } from "./utils/general.js";
 
 export function updatePokedexPage(POKEMONS_PER_PAGE = 20, pageIndex = 0) {
   getPokemons(POKEMONS_PER_PAGE, pageIndex).then((pokemons) => {
     const totalPokemons = pokemons.count;
     const { totalPages, currentPage } = pokedexPaginationValues(totalPokemons, pageIndex, POKEMONS_PER_PAGE);
-    updatePagination(POKEMONS_PER_PAGE, pageIndex, totalPages, currentPage, updatePokedexPage);
+    setupPagination(POKEMONS_PER_PAGE, pageIndex, totalPages, currentPage, updatePokedexPage);
   });
 }
