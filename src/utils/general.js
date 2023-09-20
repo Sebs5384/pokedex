e53bef8ab4745ecc4c2be432ba9591a9976f9456
Sebs5384 <operationmadjackal@hotmail.com>
2023-09-20 20:50:59 -0300

    Add toUpperCase function
    This will parse our pokemon names to upperCase on its first letter of each phrase

--- src/utils/general.js
+++ src/utils/general.js
@@ -6,9 +6,16 @@ export function calculatePaginationValues(totalItems, pageIndex, itemsPerPage) {
 }
 
 export function getPokemonNames(list) {
-  return list.map((item) => item.name);
+  return list.map((item) => firstLetterToUpperCase(item.name));
 }
 
 export function getPokemonIds(list) {
   return list.map((item) => item.url.split("/")[6]);
+}
+
+function firstLetterToUpperCase(string) {
+  return string
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join("-");
 }
