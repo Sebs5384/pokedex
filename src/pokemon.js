bd9e6158f6b6c6f5d08ff857a9334031c76ea5ea
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-19 18:45:46 -0600

    Add getPokemonNames/Ids
    These functions will be in charge of parsing the objects coming from our API call

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,11 +1,14 @@
 import { getPokemons } from "./api/pokemon.js";
 import { setupPagination } from "./ui/pagination.js";
-import { calculatePaginationValues as pokedexPaginationValues } from "./utils/general.js";
+import { calculatePaginationValues as getPokedexPaginationValues, getPokemonNames, getPokemonIds } from "./utils/general.js";
 
 export function updatePokedexPage(POKEMONS_PER_PAGE = 20, pageIndex = 0) {
   getPokemons(POKEMONS_PER_PAGE, pageIndex).then((pokemons) => {
     const totalPokemons = pokemons.count;
-    const { totalPages, currentPage } = pokedexPaginationValues(totalPokemons, pageIndex, POKEMONS_PER_PAGE);
+    const pokemonNames = getPokemonNames(pokemons.results);
+    const pokemonIds = getPokemonIds(pokemons.results);
+    const { totalPages, currentPage } = getPokedexPaginationValues(totalPokemons, pageIndex, POKEMONS_PER_PAGE);
+
     setupPagination(POKEMONS_PER_PAGE, pageIndex, totalPages, currentPage, updatePokedexPage);
   });
 }
--- src/utils/general.js
+++ src/utils/general.js
@@ -4,3 +4,11 @@ export function calculatePaginationValues(totalItems, pageIndex, itemsPerPage) {
 
   return { totalPages, currentPage };
 }
+
+export function getPokemonNames(list) {
+  return list.map((item) => item.name);
+}
+
+export function getPokemonIds(list) {
+  return list.map((item) => item.url.split("/")[6]);
+}
