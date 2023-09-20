b5badd84540fed531cf57def1611bd5738e90f1b
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-20 17:32:39 -0600

    Update cards
    We making the structure of the cards that will be displayed upon the program running

--- src/ui/cards.js
+++ src/ui/cards.js
@@ -1,1 +1,26 @@
+export function displayPokemonCards(pokemons, ids, sprites) {
+  createCards(pokemons, ids, sprites);
+}
 
+function createCards(pokemons, ids, sprites) {
+  const $container = document.querySelector("#cards-container");
+
+  $container.innerHTML = "";
+  pokemons.forEach((pokemon, index) => {
+    const id = ids[index];
+    const sprite = sprites[index];
+    const $card = document.createElement("div");
+    const $sprite = document.createElement("img");
+    const $name = document.createElement("strong");
+
+    $card.className = "card col-2 card-border";
+    $sprite.className = "card-img-top";
+    $name.className = "card-body text-center";
+
+    $sprite.src = sprite;
+    $name.textContent = `#${id} - ${pokemon}`;
+    $card.appendChild($sprite);
+    $card.appendChild($name);
+    $container.appendChild($card);
+  });
+}
