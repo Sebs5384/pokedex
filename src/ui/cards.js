d60d5dd08cf4a6b66cf8804b7324287e34ddb7da
Sebs5384 <operationmadjackal@hotmail.com>
2023-09-20 20:51:41 -0300

    Handles 404 not found on image
    The card sprites that returns 404 as response will now display a new image that is fixed in our img folder

--- src/ui/cards.js
+++ src/ui/cards.js
@@ -14,11 +14,14 @@ function createCards(pokemons, ids, sprites) {
     const $name = document.createElement("strong");
 
     $card.className = "card col-2 card-border";
-    $sprite.className = "card-img-top";
     $name.className = "card-body text-center";
+    $name.style = "font-size: 13px";
+    $name.textContent = `${pokemon.replace(/-/g, " ")}`;
 
+    $sprite.className = "card-img-top";
     $sprite.src = sprite;
-    $name.textContent = `#${id} - ${pokemon}`;
+    $sprite.onerror = () => ($sprite.src = "img/404-shocked.png");
+
     $card.appendChild($sprite);
     $card.appendChild($name);
     $container.appendChild($card);
