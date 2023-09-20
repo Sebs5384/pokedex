b050482ab48da691a09b2ea51a3037d961d490fc
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-20 17:33:14 -0600

    Add displayPokemonCards
    This will be the function that display the pokemon name, id and img in a card

--- src/pokemon.js
+++ src/pokemon.js
@@ -1,5 +1,6 @@
-import { getPokemons } from "./api/pokemon.js";
+import { getPokemons, getPokemonSprite } from "./api/pokemon.js";
 import { setupPagination } from "./ui/pagination.js";
+import { displayPokemonCards } from "./ui/cards.js";
 import { calculatePaginationValues as getPokedexPaginationValues, getPokemonNames, getPokemonIds } from "./utils/general.js";
 
 export function updatePokedexPage(POKEMONS_PER_PAGE = 20, pageIndex = 0) {
@@ -7,8 +8,10 @@ export function updatePokedexPage(POKEMONS_PER_PAGE = 20, pageIndex = 0) {
     const totalPokemons = pokemons.count;
     const pokemonNames = getPokemonNames(pokemons.results);
     const pokemonIds = getPokemonIds(pokemons.results);
+    const pokemonSprites = pokemonIds.map((id) => getPokemonSprite(id));
     const { totalPages, currentPage } = getPokedexPaginationValues(totalPokemons, pageIndex, POKEMONS_PER_PAGE);
 
     setupPagination(POKEMONS_PER_PAGE, pageIndex, totalPages, currentPage, updatePokedexPage);
+    displayPokemonCards(pokemonNames, pokemonIds, pokemonSprites);
   });
 }
