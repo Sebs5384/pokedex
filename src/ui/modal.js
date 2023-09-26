66e1ae9ef0ea93a4fa1b8500891269839fa3f798
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-09-26 01:39:21 -0600

    Creates setupPokemonModal
    This is the main function in our modal module, it will orchestrate the creating of the modal, display and anything that has something to do with the modal.

--- src/ui/modal.js
+++ src/ui/modal.js
@@ -1,16 +1,40 @@
-export function setupPokemonModal(pokemon) {
-  const $cards = document.querySelector("#cards-container");
+export function setupPokemonModal(pokemon, species, handleClickedPokemon) {
+  handleClickedPokemon((clickedPokemon) => {
+    createPokemonModal(pokemon, species, clickedPokemon);
+    displayModal("#pokemon-modal");
+  });
+}
 
-  $cards.onclick = (event) => {
-    const clickedPokemon = event.target.dataset.id;
+function createPokemonModal(pokemon, species, clickedPokemon) {
+  pokemon(clickedPokemon).then((data) => {
+    console.log(data);
+    console.log(data.name);
+    console.log(data.stats[0].base_stat);
+    console.log(data.types[0].type.name);
+    console.log(data.sprites.other["official-artwork"].front_default);
+    console.log(data.height);
+    console.log(data.weight);
+    console.log(data.abilities[0].ability.name);
+    console.log(data.abilities[1].ability.name);
+    console.log(data.stats[1].base_stat);
+    console.log(data.stats[2].base_stat);
+    console.log(data.stats[5].base_stat);
+    console.log(data.stats[3].base_stat);
+    console.log(data.stats[4].base_stat);
+    console.log(data.types[0].type.name);
+    console.log(data.id);
+  });
 
-    if (clickedPokemon === undefined) return;
+  species(clickedPokemon).then((data) => {
+    console.log(data);
+    console.log(data.evolves_from_species.name);
+    console.log(data.flavor_text_entries[0].flavor_text);
+    console.log(data.genera[7].genus);
+  });
+}
 
-    pokemon(clickedPokemon).then((data) => {
-      console.log(data);
-      const modal = document.querySelector("#pokemon-modal");
-      const modalInstance = new bootstrap.Modal(modal);
-      modalInstance.show();
-    });
-  };
+function displayModal(modal) {
+  const $modal = document.querySelector(modal);
+  const modalInstance = new bootstrap.Modal($modal);
+  modalInstance.show();
 }
