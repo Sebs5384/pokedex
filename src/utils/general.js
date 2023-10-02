7d86ee5289d1e671cbb9c0c03255c64cf1ee3bcd
Sebs5384 <operationmadjackal@hotmail.com>
2023-10-01 22:24:21 -0300

    Cleans up modal
    We are currently working towards the cleaning of the modal attributes and how they are being pass

--- src/utils/general.js
+++ src/utils/general.js
@@ -11,19 +11,19 @@ export function getPageData(getPokemons, POKEMONS_PER_PAGE, pageIndex) {
 
 export function getPokemonData(pokemon, species, selectedPokemon) {
   return Promise.all([pokemon(selectedPokemon), species(selectedPokemon)]).then(([pokemon, species]) => {
-    const pokemonSkills = pokemon.abilities.map((ability) => ability.ability.name);
-    const pokemonStats = pokemon.stats.map((stat) => stat.base_stat);
-    const pokemonTypes = pokemon.types.map((type) => type.type.name);
+    const pokemonSkills = getPokemonSkills(pokemon.abilities);
+    const pokemonStats = getPokemonStats(pokemon.stats);
+    const pokemonTypes = getPokemonTypes(pokemon.types);
     const pokemonHeight = convertDecimeterToFeet(pokemon.height);
     const pokemonWeight = convertGramToLb(pokemon.weight);
     const pokemonName = pokemon.name;
     const pokemonSprite = pokemon.sprites.other["official-artwork"].front_default;
     const previousEvolutionName = species.evolves_from_species ? `Evolves from ${species.evolves_from_species.name}` : "Basic Pokemon";
     const previousEvolutionId = species.evolves_from_species ? species.evolves_from_species.url.split("/")[6] : "None";
-    const pokemonText = species.flavor_text_entries[0].flavor_text;
+    const pokemonDescription = getEnglishDescription(species.flavor_text_entries);
     const pokemonGenus = species.genera[7].genus;
 
-    return { pokemonSkills, pokemonHeight, pokemonName, pokemonSprite, pokemonStats, pokemonTypes, pokemonWeight, previousEvolutionName, previousEvolutionId, pokemonText, pokemonGenus };
+    return { pokemonSkills, pokemonHeight, pokemonName, pokemonSprite, pokemonStats, pokemonTypes, pokemonWeight, previousEvolutionName, previousEvolutionId, pokemonDescription, pokemonGenus };
   });
 }
 
@@ -42,12 +42,44 @@ function convertDecimeterToFeet(meters) {
   return (meters * 0.328084).toFixed(2);
 }
 
+function getEnglishDescription(entry) {
+  const textEntries = entry;
+  const englishEntry = textEntries.find((entry) => entry.language.name === "en");
+
+  return englishEntry ? englishEntry.flavor_text : "";
+}
+
+function getPokemonSkills(pokemonSkills) {
+  return {
+    firstSkill: pokemonSkills[0].ability.name,
+    secondSkill: pokemonSkills[1].ability.name,
+  };
+}
+
 function getPokemonNames(list) {
   return list.map((item) => parsePokemonName(item.name));
 }
 
 function getPokemonIds(pokemons) {
   return pokemons.map((pokemon) => pokemon.url.split("/")[6]);
+}
+
+function getPokemonStats(pokemonStats) {
+  return {
+    hp: pokemonStats[0].base_stat,
+    attack: pokemonStats[1].base_stat,
+    defense: pokemonStats[2].base_stat,
+    spAtk: pokemonStats[3].base_stat,
+    spDef: pokemonStats[4].base_stat,
+    speed: pokemonStats[5].base_stat,
+  };
+}
+
+function getPokemonTypes(pokemonTypes) {
+  return {
+    mainType: pokemonTypes[0].type.name,
+    secondaryType: pokemonTypes[1].type.name,
+  };
 }
 
 function parsePokemonName(pokemonName) {
