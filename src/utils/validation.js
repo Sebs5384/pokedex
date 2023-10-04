2d7c408d131f25e8c170de8429418481d94f8fc0
Sebs5384 <operationmadjackal@hotmail.com>
2023-10-04 02:43:26 -0300

    Add validations
    Fixing a small bug that allowed the user to go to page 0 and 66

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,8 @@
 export function validatePageSearchBox(page, totalPages) {
   if (/^$/.test(page)) return "This field cannot be empty";
   if (!/^[^A-Z-a-z]+$/.test(page)) return "Only numeric characters are allowed";
-  if (page > totalPages) return `Page Nº${page} does not exist, only ${totalPages - 1} out there`;
+  if (page > totalPages - 1) return `Page Nº${page} does not exist, only ${totalPages - 1} out there`;
+  if (page === "0") return "There's no such thing as page 0, go catch a pokemon instead !";
 
   return true;
 }
