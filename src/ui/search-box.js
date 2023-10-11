6f05ff3f765b59af3b66146839f6f9610d20a837
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-10-11 03:54:07 -0600

    Creates search-box module
    This holds the logic of our search-box feature

--- src/ui/search-box.js
+++ src/ui/search-box.js
@@ -1,3 +1,45 @@
-export function setupPokedexSearchBox() {
-    
+export function handleSearchBoxClick(clicked) {
+  const $searchBox = document.querySelector("#pokedex-search-box");
+  const $pokemonList = document.querySelector("#pokedex-search-list");
+
+  $searchBox.onclick = () => {
+    if ($pokemonList.innerHTML !== "") return;
+
+    clicked();
+  };
+}
+
+export function createPokemonList(pokemons, pokemonIds) {
+  const $pokemonList = document.querySelector("#pokedex-search-list");
+
+  pokemons.forEach((name, index) => {
+    const $list = document.createElement("li");
+    const $item = document.createElement("a");
+
+    $item.className = "dropdown-item";
+    $item.textContent = `${name}`;
+    $item.dataset.id = pokemonIds[index];
+
+    $list.appendChild($item);
+    $pokemonList.appendChild($list);
+  });
+}
+
+export function handleSearchInput(search) {
+  const $searchBox = document.querySelector("#pokedex-search-box");
+
+  $searchBox.oninput = () => {
+    const pokemonQuery = $searchBox.value.toLowerCase();
+
+    search(pokemonQuery);
+  };
+}
+
+export function filterPokemonsName(pokemonQuery) {
+  const $pokemonList = document.querySelectorAll("#pokedex-search-list li a");
+
+  $pokemonList.forEach((pokemon) => {
+    const pokemonName = pokemon.textContent.toLowerCase();
+    pokemon.style.display = pokemonName.includes(pokemonQuery) ? "block" : "none";
+  });
 }
