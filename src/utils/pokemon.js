d0d3110273f9e64bddf24bd453be79b202ff03ac
Sebs5384 <operationmadjackal@hotmail.com>
2023-10-12 13:50:19 -0300

    Delete getPokemonsData and getCaughtPokemonData
    Since we divided getPokemonsData responsabilities with getPageData now we can use it instead of getPokemonsData and getCaughtPokemonsData which were doing the same but with extra-steps

--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -10,20 +10,18 @@ import {
   getEnglishDescription,
   convertGramToLb,
   convertDecimeterToFeet,
-  calculatePaginationValues,
   advantageChart,
+  calculatePaginationValues,
   catchPokemon,
 } from "./general.js";
 
-export function getPageData(getPokemons, getPokemonSprite, POKEMONS_PER_PAGE, pageIndex) {
+export function getPokemonsData(getPokemons, POKEMONS_PER_PAGE, pageIndex) {
   return getPokemons(POKEMONS_PER_PAGE, pageIndex).then((pokemons) => {
     const totalPokemons = pokemons.count;
     const pokemonNames = getPokemonNames(pokemons.results);
     const pokemonIds = getPokemonIds(pokemons.results);
-    const pokemonSprites = pokemonIds.map((id) => getPokemonSprite(id, "other/official-artwork/"));
-    const { totalPages, currentPage } = calculatePaginationValues(totalPokemons, pageIndex, POKEMONS_PER_PAGE);
 
-    return { pokemonNames, pokemonIds, totalPages, currentPage, pokemonSprites };
+    return { pokemonNames, pokemonIds, totalPokemons };
   });
 }
 
@@ -49,33 +47,8 @@ export function getPokemonData(pokemon, species, getSprite, selectedPokemon) {
   });
 }
 
-export function getCaughtPokemonData(getPokemons, pokemon, species, getPokemonSprite, limit, offset) {
-  return getPokemons(limit, offset).then((pokemons) => {
-    const caughtPokemon = catchPokemon(pokemons);
+export function getPageData(pageData, pageIndex, itemsPerPage) {
+  const { totalPages, currentPage } = calculatePaginationValues(pageData, pageIndex, itemsPerPage);
 
-    return pokemon(caughtPokemon).then((caughtPokemon) => {
-      const id = caughtPokemon.id;
-      const name = getPokemonMainName(caughtPokemon.name);
-      const height = convertDecimeterToFeet(caughtPokemon.height);
-      const weight = convertGramToLb(caughtPokemon.weight);
-      const pokemonSprite = getPokemonSprite(id);
-      const speciesName = caughtPokemon.species.name;
-
-      return species(speciesName).then((caughtPokemonSpecies) => {
-        const evolutionData = getPreviousEvolutionData(caughtPokemonSpecies);
-        const description = getEnglishDescription(caughtPokemonSpecies.flavor_text_entries);
-
-        return { id, name, height, weight, description, evolutionData, pokemonSprite };
-      });
-    });
-  });
-}
-
-export function getPokemonsData(getPokemons, limit, offset) {
-  return getPokemons(limit, offset).then((pokemons) => {
-    const names = getPokemonNames(pokemons.results);
-    const ids = getPokemonIds(pokemons.results);
-
-    return { names, ids };
-  });
+  return { totalPages, currentPage };
 }
