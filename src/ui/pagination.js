93061ec14261f98ccde67ed3d6b532facd20eb87
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2023-10-21 03:07:04 -0600

    Update quotes
    Using single quotes now instead of double

--- src/ui/pagination.js
+++ src/ui/pagination.js
@@ -1,20 +1,20 @@
 function displayCurrentPagination(currentPage, totalPages) {
-  const $pages = document.querySelectorAll(".paginator-page");
-  const $navigationButtons = document.querySelectorAll(".paginator-button");
+  const $pages = document.querySelectorAll('.paginator-page');
+  const $navigationButtons = document.querySelectorAll('.paginator-button');
   const $previousButton = $navigationButtons[0];
   const $nextButton = $navigationButtons[1];
 
-  $previousButton.classList.add(currentPage === 1 ? "disabled" : $previousButton.classList.remove("disabled"));
-  $nextButton.classList.add(currentPage === totalPages - 1 ? "disabled" : $nextButton.classList.remove("disabled"));
+  $previousButton.classList.add(currentPage === 1 ? 'disabled' : $previousButton.classList.remove('disabled'));
+  $nextButton.classList.add(currentPage === totalPages - 1 ? 'disabled' : $nextButton.classList.remove('disabled'));
 
   $pages.forEach((page, index) => {
     const pageIndex = index + 1;
-    page.classList.remove(pageIndex >= currentPage - 1 && pageIndex <= currentPage + 3 ? "hidden" : page.classList.add("hidden"));
+    page.classList.remove(pageIndex >= currentPage - 1 && pageIndex <= currentPage + 3 ? 'hidden' : page.classList.add('hidden'));
   });
 }
 
 function handlePaginationChanges(POKEMONS_PER_PAGE, pageIndex, currentPage, setNewPage) {
-  const $buttons = document.querySelector("#paginator-container");
+  const $buttons = document.querySelector('#paginator-container');
   let nextPageIndex = POKEMONS_PER_PAGE;
   let previousPageIndex = pageIndex;
 
@@ -22,17 +22,17 @@ function handlePaginationChanges(POKEMONS_PER_PAGE, pageIndex, currentPage, setN
     event.preventDefault();
     const $clickedButton = event.target;
 
-    if ($clickedButton.classList.contains("paginator-page")) {
+    if ($clickedButton.classList.contains('paginator-page')) {
       const newCurrentPage = Number($clickedButton.innerText);
       const newPageIndex = (newCurrentPage - 1) * POKEMONS_PER_PAGE;
 
       setNewPage(POKEMONS_PER_PAGE, newPageIndex);
-    } else if ($clickedButton.classList.contains("paginator-button")) {
+    } else if ($clickedButton.classList.contains('paginator-button')) {
       const clickedButton = $clickedButton.innerText;
       nextPageIndex = currentPage * POKEMONS_PER_PAGE;
       previousPageIndex = (currentPage - 2) * POKEMONS_PER_PAGE;
 
-      if (clickedButton === "Previous") {
+      if (clickedButton === 'Previous') {
         setNewPage(POKEMONS_PER_PAGE, previousPageIndex);
       } else {
         setNewPage(POKEMONS_PER_PAGE, nextPageIndex);
@@ -42,11 +42,11 @@ function handlePaginationChanges(POKEMONS_PER_PAGE, pageIndex, currentPage, setN
 }
 
 function handleErrorPopUp(message) {
-  const $paginatorSearchBox = document.querySelector("#search-page-input");
+  const $paginatorSearchBox = document.querySelector('#search-page-input');
 
   // eslint-disable-next-line no-undef
   const $popover = new bootstrap.Popover($paginatorSearchBox, {
-    container: "body",
+    container: 'body',
     html: true,
     content: message,
   });
@@ -59,13 +59,13 @@ function handleErrorPopUp(message) {
 }
 
 function handlePaginationSearch(POKEMONS_PER_PAGE, totalPages, setNewPage, pageValidation) {
-  const $pageSearcher = document.querySelector("#search-page-input");
+  const $pageSearcher = document.querySelector('#search-page-input');
 
   $pageSearcher.onkeydown = (searcher) => {
     const selectedPage = $pageSearcher.value;
     const pageIndex = (selectedPage - 1) * POKEMONS_PER_PAGE;
 
-    if (searcher.key === "Enter") {
+    if (searcher.key === 'Enter') {
       const validPage = pageValidation(selectedPage, totalPages);
       if (validPage === true) {
         setNewPage(POKEMONS_PER_PAGE, pageIndex);
@@ -77,30 +77,30 @@ function handlePaginationSearch(POKEMONS_PER_PAGE, totalPages, setNewPage, pageV
 }
 
 function createPaginator(totalPages) {
-  const $pageContainer = document.querySelector("#paginator-container");
+  const $pageContainer = document.querySelector('#paginator-container');
 
-  if ($pageContainer.innerHTML !== "") return;
+  if ($pageContainer.innerHTML !== '') return;
 
   for (let i = 0; i <= totalPages; i += 1) {
-    const $item = document.createElement("li");
-    const $page = document.createElement("a");
+    const $item = document.createElement('li');
+    const $page = document.createElement('a');
 
-    $item.className = "page-item";
-    $page.className = "page-link card-border paginator-text";
-    $page.href = "#";
+    $item.className = 'page-item';
+    $page.className = 'page-link card-border paginator-text';
+    $page.href = '#';
 
     if (i === 0) {
-      $page.textContent = "Previous";
-      $page.dataset.cy = "previous-page-button";
-      $page.classList.add("paginator-button");
+      $page.textContent = 'Previous';
+      $page.dataset.cy = 'previous-page-button';
+      $page.classList.add('paginator-button');
     } else if (i === totalPages) {
-      $page.textContent = "Next";
-      $page.dataset.cy = "next-page-button";
-      $page.classList.add("paginator-button");
+      $page.textContent = 'Next';
+      $page.dataset.cy = 'next-page-button';
+      $page.classList.add('paginator-button');
     } else {
       $page.textContent = i;
       $page.dataset.cy = `page-${i}`;
-      $page.classList.add("paginator-page");
+      $page.classList.add('paginator-page');
     }
 
     $item.appendChild($page);
