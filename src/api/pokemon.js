5a4ec67e25f0c991e49e76bafa9efd7d5b5ca9d1
Sebs5384 <120617688+Sebs5384@users.noreply.github.com>
2024-02-08 17:16:35 -0600

    Removes console.log

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,7 +1,6 @@
 const URL = 'https://pokeapi.co/api/v2';
 
 export async function getPokemon(id) {
-  console.log(id);
   const pokemonURL = `${URL}/pokemon/${id}`;
 
   // eslint-disable-next-line no-return-await
